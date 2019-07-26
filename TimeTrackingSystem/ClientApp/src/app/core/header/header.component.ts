009eaf9968b7a0379ca21f4b7fb66dcc3425ba5a
Igor Dobosz <igordobosz@gmail.com>
2019-07-26 17:54:39 +0200

    Pipe test

--- TimeTrackingSystem/ClientApp/src/app/core/header/header.component.ts
+++ TimeTrackingSystem/ClientApp/src/app/core/header/header.component.ts
@@ -1,41 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../authentication/authentication.service';
-import { Router, Route, ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "../authentication/authentication.service";
+import { Router, Route, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
   logged = false;
   returnUrl: string;
-  constructor(private authenticationService: AuthenticationService, private router: Router, private route: ActivatedRoute) {
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     if (this.authenticationService.currentUserValue) {
-      this.router.navigate(['/']);
-   }
+      this.router.navigate(["/"]);
+    }
   }
 
   ngOnInit() {
-    this.authenticationService.currentUser.subscribe(s => this.logged = s != null)
-    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.authenticationService.currentUser.subscribe(
+      s => (this.logged = s != null)
+    );
+    this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
   }
 
-  login(){
-    this.authenticationService.login("igordobosz@gmail.com", "IDobosz").subscribe(user => {
-      if (user.token)
-      {
-        this.router.navigate([this.returnUrl]);
-      }
-      else
-      {
-        alert('errror');
-      }})
+  login() {
+    this.authenticationService
+      .login("igordobosz@gmail.com", "IDobosz")
+      .subscribe(user => {
+        if (user.token) {
+          this.router.navigate([this.returnUrl]);
+        } else {
+          alert("errror");
+        }
+      });
   }
 
-  logout(){
+  logout() {
     this.authenticationService.logout();
-    this.router.navigate(['/login']);
+    this.router.navigate(["/login"]);
   }
-
 }
