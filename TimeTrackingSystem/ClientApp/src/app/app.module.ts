c8844fcedab4317c5aa3a0c2c4ea7e0fd761ee3e
Igor Dobosz <igordobosz@gmail.com>
2019-11-07 12:56:41 +0100

    Api url fix

--- TimeTrackingSystem/ClientApp/src/app/app.module.ts
+++ TimeTrackingSystem/ClientApp/src/app/app.module.ts
@@ -1,37 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './modules/main/home/home.component';
-import { MainModule } from './modules/main/main.module';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
-import { AuthGuard } from './core/guards/auth.guard';
-import { LoginComponent } from './modules/main/login/login.component';
-import { AppRootRoutes } from './app.routes';
-import { EmployeeModule } from './modules/employee/employee.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AppRootRoutes } from './app.routes';
+import { API_BASE_URL } from './core/api.generated';
+import { CoreModule } from './core/core.module';
+import { AuthGuard } from './core/guards/auth.guard';
+import { EmployeeModule } from './modules/employee/employee.module';
+import { HomeComponent } from './modules/main/home/home.component';
+import { LoginComponent } from './modules/main/login/login.component';
+import { MainModule } from './modules/main/main.module';
+import { SharedModule } from './shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule,
-    CoreModule,
-    MainModule,
-    SharedModule,
-    EmployeeModule,
-    RouterModule.forRoot(AppRootRoutes)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        FormsModule,
+        CoreModule,
+        MainModule,
+        SharedModule,
+        EmployeeModule,
+        RouterModule.forRoot(AppRootRoutes),
+    ],
+    providers: [{ provide: API_BASE_URL, useValue: '' },
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
